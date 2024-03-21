75bcede7ab25c1cfc3a0506355bb2b54104af79e
rachit-j <rjaiswal.sd.77@gmail.com>
2024-03-21 10:41:28 -0700

    It works but i had to delete everything. DONT MERGE

---
+++ src/main/java/com/nighthawk/spring_portfolio/Greeting.java
@@ -0,0 +1,18 @@
+package com.nighthawk.spring_portfolio;
+
+public class Greeting {
+
+	private String content;
+
+	public Greeting() {
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
--- src/main/java/com/nighthawk/spring_portfolio/mvc/enemy/EnemyJPA.java
+++
@@ -1,9 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.enemy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnemyJPA extends JpaRepository<Enemy, Long> {
-    Enemy findByName(String name); // Update method name to match the property 'name'
-}
